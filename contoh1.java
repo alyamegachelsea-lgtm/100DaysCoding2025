fb0f47737aa3afd6be21a625cd0ef5335efdac39
alyamegachelsea-lgtm <alyamegachelsea@gmail.com>
2025-09-07 14:50:09 +0800

    Create contoh1.java

---
+++ contoh1.java
@@ -0,0 +1,7 @@
+public class Main {
+    
+    public static void main(String[] args){
+        double desimal = 17;
+        System.out.println("desimal="+ desimal);
+        int bulat = -5;
+        System.out.println("bulat="+ bulat);
