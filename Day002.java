10604e4e08679eb9e7e2e705f19a898ebaac6311
alyamegachelsea-lgtm <alyamegachelsea@gmail.com>
2025-09-09 08:40:41 +0800

    Create Day002.java

---
+++ Day002.java
@@ -0,0 +1,9 @@
+public class Chelsea {
+  public static void main(String[] argh) {
+  System.out.println("nama saya chelsea");
+  System.out.print("hi saya chelsea \n");
+  System.out.printf("nama saya %s, umur saya %d", "chelsea", 17); 
+
+    
+  }
+}
