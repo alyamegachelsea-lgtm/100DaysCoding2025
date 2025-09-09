92e476002ff45a6a56eaafe571a2974baa6d6eab
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-10 00:42:18 +0800

    Create Day003.java

---
+++ Day003.java
@@ -0,0 +1,11 @@
+public class chelsea {
+ public static void main(String[] args) {
+//  int plus = 8;
+//  int class = 8;
+//  int wann = plus * class;
+//  System.out.println(wann);
+   
+ }
+
+  
+}
