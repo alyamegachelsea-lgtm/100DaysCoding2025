0d265c58fa994b0f08176e6c893ac02bc2842876
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-10 09:00:31 -0700

    Day004.java

---
+++ Day004.java
@@ -0,0 +1,13 @@
+public class Day004Java {
+    public static void main(String[] args) {
+    String nama,alamat;
+    nama = "chelsea";
+    alamat = "kebumen";
+    int umur = 17;
+        System.out.println("\nnama saya \t\t :" + nama);
+        System.out.println("\nsaya berasal dari \t :" + alamat);
+        System.out.println("\numur saya saat ini \t :" + umur);     
+    }
+    
+    
+}
