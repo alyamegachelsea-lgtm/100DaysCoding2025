985be68e24c2ebf1f122da6fc42a8901e5b70690
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-12 02:36:29 -0700

    Create Day005.java

---
+++ Day005.java
@@ -0,0 +1,23 @@
+public class Day005{
+public static voil main(String[] args) {
+      byte angka1 = 127;
+      short angka2 = 32700;
+      int angka3 = 210030509;
+      long angka4 = 1000000000;
+        System.out.println("Tipe data byte :" +angka1);
+        System.out.println("Tipe data short:" +angka2);
+        System.out.println("Tipe data int  :" +angka3);
+        System.out.println("Tipe data long :" +angka4);
+
+
+
+
+
+  
+
+
+  
+  }
+
+
+}
