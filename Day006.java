964f62b4858937d4dd2da743403d92fbabb1acba
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-12 17:04:38 -0700

    Create Day006.java

---
+++ Day006.java
@@ -0,0 +1,8 @@
+public class DAY006;{
+public static void main(String[] args) {
+      double tinggi = 150;
+      float bb = 40;
+        System.out.println("Tinggi saya : "+tinggi);
+        System.out.println("berat badan saya: "+bb);
+ }
+}
