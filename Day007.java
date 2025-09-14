ffb64e2a6ccf511ccdd0255fb7f48481038cf370
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-13 21:19:12 -0700

    Create Day007.java

---
+++ Day007.java
@@ -0,0 +1,10 @@
+public class Day007 {
+  public static void main(String[] args) { "+"chelsea");
+          char nama = 'C';
+          boolean up =true;
+          System.out.println("Nama saya "+"chelsea");
+          System.out.println(up);
+          
+
+  }
+  }
