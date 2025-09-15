37ffb65b911cbeb2308eb2cc1208e3bfcc80f127
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-14 20:21:01 -0700

    Create Day008.java

---
+++ Day008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Day008 {
+  public static void main(String[] args) {
+        String nama = "Chelsea";
+        String umur = "17";
+        String prodi = "Informatika";
+        System.out.println("Nama saya\t: "+nama);
+        System.out.println("Umur saya\t: "+umur);
+        System.out.println("Prodi saya\t: "+prodi);
+
+  }
+  }
