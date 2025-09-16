9129f63ba6c3243dd7e5428f19b794e76086926f
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-16 06:40:40 -0700

    Create Day009.java

---
+++ Day009.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Day004Java {
+    public static void main(String[] args) {  
+//      DEKLARASI VARIABEL
+      
+        String nama;
+        int umur ;
+        double tinggi;
+//      INISIALISASI VARIABEL 
+      
+        nama = "Chelsea";
+        umur = 17;
+        tinggi = 150.0;
+ //     UPDATE VARIABEL     
+      
+        nama = "CHELSEA";
+        umur = 17;
+        tinggi = 150.0;
+        System.out.println("Nama saya: "+nama);
+        System.out.println("Umur saya: "+umur);
+        System.out.println("Tinggi saya: "+tinggi);
+     
+
+
+
+
+      
+    }
+}
