48bfeee5be07eb6000ecf3d7a3668b2353cd2f3f
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-16 09:00:06 -0700

    Create Day010.java

---
+++ Day010.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day004Java {
+public static void main(String[] args) {      
+      
+final String nama = "CHELSEA";
+final byte umur = 17;
+final int tinggi = 150;
+final double bb = 40;
+ System.out.println("Nama member: "+nama);
+ System.out.println("Umur member: "+umur);
+ System.out.println("Tinggi member: "+tinggi);
+ System.out.println("Berat badan member: "+bb);
+    }
+}
