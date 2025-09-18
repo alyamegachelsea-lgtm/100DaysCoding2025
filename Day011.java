64d2f7d54c126684711480f653c284ec6f9386ce
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-17 23:08:26 -0700

    Update Day011.java

--- Day011.java
+++ Day011.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 public class Day011Java {
   public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan bilangan: ");
-        String y = sc .nextLine();
-
+        System.out.print("Masukkan nama saya : ");
+        String nama = sc .nextLine(); 
+        System.out.print("nim saya : ");
+        String nim = sc.nextLine();
+        System.out.print("Nama saya : "+ nama);
+        System.out.print("Nim saya : "+ nim);
+        
     }
 }
         
