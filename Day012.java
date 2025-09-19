e9d5c7965f907b6a13d4022f81d415f9700ebfbd
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-19 02:54:16 -0700

    Create Day012.java

---
+++ Day012.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Day012.Java {
+    public static void main(String[] args) {
+      
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Nama: ");
+        String nama = input.nextLine();
+        
+        System.out.print("Umur? ");
+        int umur = input.nextInt();
+        input.nextLine();
+       
+       
+        System.out.print("tinggi? ");  
+        double tinggi = input.nextDouble();
+        
+        System.out.println("nama saya :"+ nama);
+        System.out.print("umur saya :"+ umur); 
+        System.out.printf("\ntinngi saya %s" ,tinggi);
+        }
+    
+}
