48b4e16e09c27b0f8607fac2bfc9cc5beeb290f9
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-20 23:24:57 -0700

    Create Day014.java

---
+++ Day014.java
@@ -0,0 +1,17 @@
+package day014java;
+
+public class Day014java {
+    public static void main(String[] args) {
+      String no = "125";
+      double angka = Double.parseDouble(no);
+        System.out.println(angka);
+
+      String nomer = "67";
+      int bilangan = Integer.parseInt(nomer);
+        System.out.println(bilangan);
+      
+
+
+    }
+    
+}
