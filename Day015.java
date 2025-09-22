209b1f741569a242e716ed6d4acc6cb01168c010
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-22 05:19:33 -0700

    Create Day015.java

---
+++ Day015.java
@@ -0,0 +1,15 @@
+package day015java;
+
+public class Day015java {
+    public static void main(String[] args) {
+        int chel = 2;
+        int clas = 20;
+        int plus = 2 + 20;
+        System.out.println(plus);
+        int glas = 4;
+        int korr = 8;
+        int iyy = glas - korr;
+        System.out.println(iyy);
+    }
+    
+}
