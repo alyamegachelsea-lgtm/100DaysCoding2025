1fba444dd818f376e1b4a74e20ebfa322ad254a4
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-23 05:44:00 -0700

    Create Day016.java

---
+++ Day016.java
@@ -0,0 +1,22 @@
+package javaapplication18;
+
+import java.util.Scanner;
+
+public class JavaApplication18 {
+
+     public static void main(String[] args) {
+         Scanner cell = new Scanner(System.in);
+         System.out.print("Masukkan angka pertama: ");
+         int angka1 = cell.nextInt();
+         System.out.print("Masukkan angka kedua: ");
+         int angka2 = cell.nextInt();
+         System.out.println();
+         System.out.println("Hasil perkalian: "+angka1*angka2);
+         System.out.println("Hasil pembagian: "+angka1/angka2);
+             
+            
+            
+            
+    }
+    
+}
