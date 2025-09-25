f89a4ac358a479dc82e238f1af21dfc4649355e5
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-24 20:32:28 -0700

    Create Day018.java

---
+++ Day018.java
@@ -0,0 +1,9 @@
+package Day018;
+public class Day018java {
+    public static void main(String[] args) {
+       //konversi dari kecil kebesar
+        short a = 50;
+        long b = a;
+        System.out.println(b);
+    }
+}
