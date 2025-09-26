a5586243b5e34ba8605079d7bd5598554d3e998e
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-26 16:23:14 -0700

    Create Day020.java

---
+++ Day020.java
@@ -0,0 +1,16 @@
+public class Day020java{
+  public static void main(String[] args){
+  
+    //tipe data primitif
+       int a = 2365;
+       double b = 35.0; 
+       
+    //cara membuat primitif ke string
+       String c = Integer.toString(a);
+       String d = Double.toString(b);
+    
+    //cetak
+       System.out.println("Integer to String : "+ c);
+       System.out.println("Double to String : "+ d);
+  }
+}
