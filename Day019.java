fcf0f9b0470d6fc37a402e944857f92aeea4a98b
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-09-26 04:07:48 -0700

    Create Day019.java

---
+++ Day019.java
@@ -0,0 +1,14 @@
+public class Day019java {
+    public static void main(String[] args) {
+      
+      //dari besar ke kecil
+        long c = 855;
+        byte d = (byte) c;
+        
+        System.out.println(c);
+        System.out.println(d);
+           
+        
+    }
+    
+}
