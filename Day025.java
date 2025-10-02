b2e69c012bef118a4f88e9d10efbace09df1d67a
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-10-02 06:01:19 -0700

    Create Day025.java

---
+++ Day025.java
@@ -0,0 +1,13 @@
+public class chell {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        
+        x++;
+        System.out.println("Hasil increment = " +x); 
+        y--;
+        System.out.println("Hasil decrement = " +y);        
+
+    }
+    
+}
