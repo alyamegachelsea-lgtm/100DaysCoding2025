5bf0a97d4bff9b309c27f9c394a90a4b617f30c7
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-10-02 17:19:05 -0700

    Create Day026.java

---
+++ Day026.java
@@ -0,0 +1,12 @@
+public class Chell {
+    public static void main (String [] args) {
+        double a = 22;
+        a +=5;
+        double b =10;
+        b /=2;
+        System.out.println(a);
+        System.out.println(b);  
+        
+    }
+    
+}
