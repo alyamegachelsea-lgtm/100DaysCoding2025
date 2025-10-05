9fa604f9dbdf3cca5525fcc061ca573f9254732e
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-10-05 05:25:29 -0700

    Create Day028.java

---
+++ Day028.java
@@ -0,0 +1,15 @@
+package Chelll;
+import java.util.Scanner;
+public class day28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nomer pertama\t: ");
+        int c = sc.nextInt();
+        System.out.print("Nomer kedua\t: ");
+        int d = sc.nextInt();
+        System.out.println("Apakah nilai a adalah b?\t "+ (d==c));
+
+        
+    }
+    
+}
