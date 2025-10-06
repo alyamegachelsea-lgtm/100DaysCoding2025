fdfceb351a6979c77f61d070a7659642c5432cf5
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-10-06 06:23:28 -0700

    Create day029.java

---
+++ day029.java
@@ -0,0 +1,11 @@
+package day029.java;
+public class Day029Java {
+    public static void main(String[] args) {
+     int a = 10;
+     int b = 20;
+        System.out.println("Apakah a < b? "+ (a>b));
+        System.out.println("Apakah a > b? "+ (a<b));
+    
+    }
+    
+}
