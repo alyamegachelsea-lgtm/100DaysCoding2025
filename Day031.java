2f56fbc007e2f639be1b671e2d2e467883063e05
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-10-08 06:29:48 -0700

    Create Day031.java

---
+++ Day031.java
@@ -0,0 +1,14 @@
+public class percabangan {
+    public static void main(String[] args) {
+   
+        int umur = 16;
+        boolean punyaATM = true;
+
+        if (umur >= 17 && punyaATM == true) {
+            System.out.println("Sudah dewasa");
+        } else {
+            System.out.println("Belum dewasa");
+        }
+
+    }
+}
