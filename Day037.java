a72d109e3686fcae5264f04d20fd929db09bb38c
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-10-14 04:11:26 -0700

    Update Day037.java

--- Day037.java
+++ Day037.java
@@ -3,13 +3,13 @@ public class if_else {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
       
-        System.out.print("masukkan bilanga bulat: ");
+        System.out.print("masukkan bilanga: ");
         int bilangan = sc.nextInt();
         
-        if(bilangan % 2 == 0){
-            System.out.println("bilangan " + bilangan + " adalah genap");
+        if(bilangan % 2 != 0){
+            System.out.println("bilangan " + bilangan + " adalah ganjil");
         
-        }else{System.out.println("bilangan " + bilangan + " adalah ganjil");        
+        }else{System.out.println("bilangan " + bilangan + " adalah genap");        
         
         }
     }
