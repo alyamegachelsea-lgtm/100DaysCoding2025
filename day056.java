4a78e83ff36bf5445a7620ca3bd99613c140d585
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-11-02 03:50:39 -0800

    Create day056.java

---
+++ day056.java
@@ -0,0 +1,11 @@
+public class DO_while {
+    public static void main(String[] args) {
+         int i = 1;
+        do {
+            System.out.println("Angka: " + i);
+            i++;
+        } while (i <= 3);
+
+    }
+            
+}
