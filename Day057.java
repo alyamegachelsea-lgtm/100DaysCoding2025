f269ed6976a2a735a57ebf6a67998e59a8995c11
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-11-03 02:27:49 -0800

    Create Day057.java

---
+++ Day057.java
@@ -0,0 +1,12 @@
+public class NestedLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Baris ke-" + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(" Kolom ke-" + j);
+            }
+        }
+    }
+}
+            
+        
