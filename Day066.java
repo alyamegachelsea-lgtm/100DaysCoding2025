834b8499e01407cbd57deb926dfe4decc8eebc31
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-11-12 03:31:17 -0800

    Create Day066.java

---
+++ Day066.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Day066 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris: ");
+        int n = sc.nextInt();
+
+        // Mencetak pola vertikal
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*");
+        
+        
+        
+        
+        
+       }
+    }
+}
