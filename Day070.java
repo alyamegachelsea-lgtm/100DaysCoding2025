3ec776ff8db0b17e6291c138429c0658ee499f15
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-11-16 04:59:29 -0800

    Add Day070.java for printing asterisks in grid

---
+++ Day070.java
@@ -0,0 +1,26 @@
+package Day070;
+
+import java.util.Scanner;
+
+public class java {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah baris (tinggi): ");
+        int baris = sc.nextInt();
+        System.out.print("Masukkan jumlah kolom (lebar): ");
+        int kolom = sc.nextInt();
+
+        for (int i = 0; i < baris; i++) {
+            for (int c = 0; c < kolom; c++) {
+                System.out.print("* ");
+            }
+            System.out.println();  
+        
+        
+        
+        
+        
+        }
+        
+    }
+}
