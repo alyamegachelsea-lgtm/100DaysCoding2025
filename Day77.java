5628b65bdc4665182017949dea42ef5d81350527
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-11-23 00:31:54 +0800

    Use intermediate variables for string operations
    Refactor string manipulations to use variables for clarity.

---
+++ Day77.java
@@ -0,0 +1,13 @@
+public class Latihansoal {
+    public static void main(String[] args) {
+        String nama = "ChelseaGalau";
+        String nama1 = nama.substring(7);
+        System.out.println(nama1);
+        String cel = "bulan lalu akuTIDAK menolak seseorang";
+        String cel1 = cel.replace("TIDAK","");
+        System.out.println(cell);
+        String anuu = "      cell gamau ingat pukis karna mantannya       ";
+        String anuu1 = anuu.trim();
+        System.out.println(anuu1);
+    }
+}    
