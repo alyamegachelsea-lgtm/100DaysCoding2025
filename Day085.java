f70b7bbb629d8d10ab0cbad5e6f376e79258b919
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-01 23:22:15 +0800

    Update Day085.java

--- Day085.java
+++ Day085.java
@@ -1,12 +1,9 @@
 package latihan_c;
-
 import java.util.Scanner;
-
 public class Latihan_c {
-
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-
         int [] angka = {10,20,30,40};       
         
         int maks = 0;
@@ -17,3 +14,5 @@ public class Latihan_c {
                 
         }
         System.out.println("angka terbesar array: "+maks);
+    }
+}
