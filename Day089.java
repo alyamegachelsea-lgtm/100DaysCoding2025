63857b00805dfcb0ca08acd59987690a979b4c75
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-05 00:11:32 +0800

    Create Day089.java

---
+++ Day089.java
@@ -0,0 +1,17 @@
+package latihan_method;
+public class Latihan_method {
+  public static void chel(){
+        
+        for (int i = 1; i < 10; i++) {
+            if (i%2==0) {
+                System.out.println(i);
+                
+                  }
+              }
+         }
+  
+  public static void main(String[] args) {
+        chel();
+    }
+    
+}
