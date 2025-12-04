a1352a3da4d62cc651e55df067912bb83c1b396b
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-04 22:19:25 +0800

    Create Day088.java

---
+++ Day088.java
@@ -0,0 +1,15 @@
+package pkg100days;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String hewan [] = {"1.kucing","2.anjing","3.kuda","4.harimau","5.tikus",};
+      
+        System.out.println("Daftar hewan: ");
+        for (String c :hewan) {
+            System.out.println(c);
+        }
+        
+    }
+    
+}
