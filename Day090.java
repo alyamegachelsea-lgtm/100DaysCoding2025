e9a3e422d3da3ef11beb1787853cf35f604b55ea
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-06 22:01:13 +0800

    Create Day090.java

---
+++ Day090.java
@@ -0,0 +1,13 @@
+package latihan_method;
+public class Latihan_method {     
+  //non void
+    public static int tambah(int angka1,int angka2){
+        return angka1+angka2;  
+    }
+   public static void main(String[] args) {
+       
+        System.out.println(tambah(2,4));
+        
+    }
+    
+}
