660c7c804f4d380b057018e1baab158ed86cfc0d
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-07 21:40:28 +0800

    Create Day091.java

---
+++ Day091.java
@@ -0,0 +1,9 @@
+package latihan_method;
+public class Latihan_method {     
+    public static String nama1(String nama1){
+        return nama1+"2007"; 
+    }
+  public static void main(String[] args) {
+        System.out.println(nama1("alya"));
+  }
+ }
