51da4b96ba49709587dd6a176fcf1151f055b357
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-08 23:28:46 +0800

    Add ContohReturn class with addition method

---
+++ Day092.java
@@ -0,0 +1,10 @@
+public class ContohReturn {
+    
+    public static int tambah(int a, int b){
+        return a + b;   // mengembalikan hasil penjumlahan
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(tambah(5, 7));
+    }
+}
