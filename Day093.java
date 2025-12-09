a53b05adc8e35bdb931f9a026bb0c17037073d4d
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-09 23:23:36 +0800

    Create Day093.java

---
+++ Day093.java
@@ -0,0 +1,12 @@
+public class Pembulatan {
+    public static void main(String[] args) {
+        double angka1 = 7.8;
+
+        System.out.println("Angka asli: " + angka1);
+        System.out.println("Ceil: " + Math.ceil(angka1));     
+        System.out.println("Floor: " + Math.floor(angka1));   
+        System.out.println("Round: " + Math.round(angka1));   
+
+        
+    }
+}
