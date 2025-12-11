b5b35a4d0291bd74dd44d969ecc4fb0c016de5e0
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-11 23:46:48 +0800

    Add CetakNama class to print names

---
+++ Day095.java
@@ -0,0 +1,11 @@
+public class CetakNama {
+  
+    public static void cetakNama(String nama) {
+        System.out.println("Halo, " + nama + "!");
+    }
+
+    public static void main(String[] args) {
+        cetakNama("Chelsea");
+        cetakNama("Alya");
+    }
+}
