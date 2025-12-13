568adb8a46b504ffcb252e723496761eecb6ecbf
Chelsea Alya Mega <alyamegachelsea@gmail.com>
2025-12-13 20:45:55 +0800

    Add Latihan_method class with pankal method

---
+++ Day097.java
@@ -0,0 +1,12 @@
+package latihan_method;
+public class Latihan_method {
+  public static int pankal(int panjang, int lebar){
+        return panjang*lebar;
+    }
+  public static void main(String[] args) {
+        
+        System.out.println(pankal(5,5));
+       
+    }
+    
+}
